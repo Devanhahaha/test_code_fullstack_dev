0c6ebaacaf7eeb2762c0aee9b084458668bf23f3
Devanhahaha <devanherdiansyah74@gmail.com>
2026-07-28 10:31:10 +0700

    desain database, set up project, migration, model, relation, integrasi spatie, install laravel api

---
+++ backend/be/app/Models/Project.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'client_id',
+        'name',
+        'deadline',
+        'status',
+    ];
+
+    public function client() {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function tasks() {
+        return $this->hasMany(Task::class);
+    }
+}
